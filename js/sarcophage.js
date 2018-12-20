eaabbd50b5dff8574d2ab053e5c955c22a3c5b4f
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2018-12-20 22:33:00 +0100

    MAJ scene anubis

--- js/sarcophage.js
+++ js/sarcophage.js
@@ -10,6 +10,7 @@
 			var windowHalfY = window.innerHeight / 2;
 
 			var object;
+			var models = [];
 			var lights = []
 
 			init();
@@ -22,12 +23,13 @@
 				document.body.appendChild( container );
 
 				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 20000 );
-				camera.position.z = -970;
+				camera.position.z = -500;
 
 				// scene
 
 				scene = new THREE.Scene();
-
+				var fogColor = new THREE.Color(0x0d0551);
+				scene.fog = new THREE.FogExp2(fogColor, 0.001);
 				// LIGHTS
 
 				var intensity = 2.0;
@@ -87,21 +89,98 @@
 
 				function loadModel() {
 					var material = new THREE.MeshLambertMaterial({color: 0x1133ff, roughness: 1, metalness: 0.5, side: THREE.DoubleSide,});
+					var specularMaterial = new THREE.MeshPhongMaterial( { color: 0x1133ff, roughness: 0.5, metalness: 0.5, side: THREE.DoubleSide, } );
+					var shinningMaterial = new THREE.MeshPhongMaterial( { 
+					color: 0x996633,
+					// envMap: envMap, // optional environment map
+					specular: 0xFFD700,
+					shininess: 150
+				} ) 
+					models.forEach((mod, i) => {
+						// mod.traverse( function ( child ) {
 
-					object.traverse( function ( child ) {
+						// if ( child.isMesh ) child.material = material;
 
-						if ( child.isMesh ) child.material = material;
+						// } );
 
-					} );
+						mod.scale.set(0.2,0.2,0.2 )
+						if (i==0) {
+							mod.traverse( function ( child ) {
+
+							if ( child.isMesh ) child.material = material;
+
+							} );
+							mod.position.y = 0;
+							mod.position.x = 0;
+							mod.rotation.y = Math.PI/2;
+							mod.rotation.z = - Math.PI/2;
+							mod.scale = THREE.Vector3(0.2,0.2,0.2 )
+
+							
+						}else if (i == 1){
+							mod.traverse( function ( child ) {
+
+							if ( child.isMesh ) child.material = shinningMaterial;
+
+							} );
+							mod.position.x = 25
+							mod.position.z = 750
+							// mod.scale.set(200,200,200)
+							
+							
+						}else if (i == 2){
+							mod.traverse( function ( child ) {
+
+							if ( child.isMesh ) child.material = shinningMaterial;
+
+							} );
+							mod.position.x = -25 
+							mod.position.z = 750
+							
+						}else if (i == 3){
+							mod.traverse( function ( child ) {
+
+							if ( child.isMesh ) child.material = shinningMaterial;
+
+							} );
+							mod.position.x = 75
+							mod.position.z = 750
+							
+						}else if (i == 4) {
+							mod.traverse( function ( child ) {
+
+							if ( child.isMesh ) child.material = shinningMaterial;
+
+							} );
+							mod.position.x = -75
+							mod.position.z = 650
+						}else if (i == 5) {
+							mod.traverse( function ( child ) {
+
+								if ( child.isMesh ) child.material = specularMaterial;
+	
+								} );
+							mod.position.x = 0
+							mod.position.y = 0
+							mod.position.z = 1200
+							mod.scale.set(0.1,0.1,0.1)
+						}
+						scene.add( mod );
+					});
+					// object.traverse( function ( child ) {
+
+					// 	if ( child.isMesh ) child.material = material;
+
+					// } );
 
 
 					/* DEBOUT */
-					object.position.y = 50;
-					object.rotation.y = Math.PI/2;
-					object.rotation.z = - Math.PI/2;
-					object.scale = THREE.Vector3(0.1,0.1,0.1 )
+					// object.position.y = 50;
+					// object.rotation.y = Math.PI/2;
+					// object.rotation.z = - Math.PI/2;
+					// object.scale = THREE.Vector3(0.1,0.1,0.1 )
 
-					scene.add( object );
+					// scene.add( object );
 
 				}
 
@@ -135,12 +214,19 @@
 				function onError() {}
 
 				var loader = new THREE.OBJLoader( manager );
+				var modelsPath = ['js/obj/sarcophage.obj', 'js/obj/hieroglyphes_coeur.obj', 'js/obj/hieroglyphes_plume.obj', 'js/obj/hieroglyphes_arbre.obj', 'js/obj/hieroglyphes_noeud.obj', 'js/obj/anubis.obj']
+				modelsPath.forEach(mod => {
+					loader.load( mod, function ( obj ) {
+						models.push(obj)
+						object = obj;
+					}, onProgress, onError );
+				});
 
-				loader.load( 'js/obj/sarcophage.obj', function ( obj ) {
+				// loader.load( 'js/obj/sarcophage.obj', function ( obj ) {
 
-					object = obj;
+				// 	object = obj;
 
-				}, onProgress, onError );
+				// }, onProgress, onError );
 
 				//
 
@@ -151,13 +237,23 @@
 				container.appendChild( renderer.domElement );
 
 				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-
+				document.addEventListener('wheel', onMouseWheel)
 				//
 
 				window.addEventListener( 'resize', onWindowResize, false );
-
+				addRiver()
 			}
+			function addRiver(){
+				
+				var geometry1 = new THREE.PlaneBufferGeometry( 200, 20000, 32 );
+				var material = new THREE.MeshBasicMaterial( {color: 0x1133ff, side: THREE.DoubleSide, transparent: true,  opacity: 0.5} );
+				var plane = new THREE.Mesh( geometry1, material );
+				plane.rotation.x = - Math.PI/-2;
+				plane.position.y -= 80
 
+				scene.add( plane );
+				
+			}
 			function onWindowResize() {
 
 				windowHalfX = window.innerWidth / 2;
@@ -169,7 +265,17 @@
 				renderer.setSize( window.innerWidth, window.innerHeight );
 
 			}
+			function onMouseWheel(e){
 
+				if (e.deltaY > 0) {
+					camera.position.z += 50
+					models[0].position.z += 50
+				}else
+				if (e.deltaY < 0) {
+					camera.position.z -= 50
+					models[0].position.z -= 50
+				} 
+			}
 			function onDocumentMouseMove( event ) {
 
 				mouseX = ( event.clientX - windowHalfX ) / 2;
@@ -182,50 +288,93 @@
 			function animate() {
 				requestAnimationFrame( animate );
 				render();
-
+				anubis();
 			}
 
 
 
 
-
-
-			function render() {
-
-				camera.position.x += ( mouseX - camera.position.x ) * .05;
-				// camera.position.y += ( - mouseY - camera.position.y ) * .05;
-
-				let timer = Date.now() / 1000;
-
-
-
 				/*** pivoter le sarcophage ***/
 				var button_go = document.getElementById('button_go');
+				var button_commence = document.getElementById('commence');
 				var bloc = document.getElementById('bloc');
 				var cadre = document.getElementById('cadre');
+				var scroll = document.getElementById('scroller');
+
+				button_commence.onclick = function(){
+					models[0].position.z -= 300;
+					// object.position.x -= 100;
+				}
 
 				button_go.onclick = function(){
 					bloc.style.display = "none";
 					button_go.style.display = "none";
+					scroll.style.animation = "fondu 3s";
 
 					/* ALLONGE */
 					camera.position.y = -300;
 					camera.position.x = 1000;
-					
-					object.position.y = -200;
-					object.rotation.y = 300;
-					object.rotation.x = 0;
-					object.rotation.z = Math.PI/6;
 
-					// cadre.style.backgroundSize = "100% 90vh";
-					// cadre.style.left = "4vh";
-					// cadre.style.right = "4vh";
-					// cadre.style.top = "5vh";
+					models[0].position.y -= 100;
+					models[0].rotation.y = 300;
+					models[0].rotation.x = 0;
+					models[0].rotation.z = Math.PI/6;
+				}
+
+			/*********** APPARITION ANUBIS ***********/
+				var cadre = document.getElementById('cadre');
+
+				function anubis(){
+					if (camera.position.z == 1000){
+						cadre.style.backgroundSize = "100% 90vh";
+						cadre.style.left = "4vh";
+						cadre.style.right = "4vh";
+						cadre.style.top = "5vh";
+
+						/* phrase 1 */
+						setTimeout(function() { 
+							$('.phrase6').fadeIn();
+							$('.interlocuteur').fadeIn();
+						}, 1000);
+						setTimeout(function() { 
+							$('.phrase6').fadeOut(); 
+						}, 7000); 
+						
+						/* phrase 2 */
+						setTimeout(function() { 
+							$('.phrase7').fadeIn();
+						}, 7300); 
+						
+						setTimeout(function() { 
+							$('.phrase7').fadeOut(); 
+						}, 14500);
+						
+						/* phrase 3 */
+						setTimeout(function() { 
+							$('.phrase8').fadeIn();
+						}, 14800);
+						
+						setTimeout(function() { 
+							$('.phrase8').fadeOut(); 
+							$('.interlocuteur').fadeOut(); 
+						}, 21800);
+						
+					}
 				}
 
 
+			function render() {
 
-				camera.lookAt( scene.position );
+				
+				camera.position.x = ( mouseX - camera.position.x ) * .01;
+				camera.position.y = ( - mouseY - camera.position.y ) * .01;
+				// camera.position.x += ( mouseX - camera.position.x ) * .05;
+				// camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+			
+
+				camera.lookAt( 0,0,99999 );
+				camera.updateMatrixWorld();
 
 				var time = Date.now() * 0.00025;
 				var d = 150;
